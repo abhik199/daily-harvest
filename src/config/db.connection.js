9208855853a485a256c8b0e92bf89c24239e2e90
abhik199 <abhishekkirar2004@gmail.com>
2024-07-31 00:34:08 +0530

    fix

--- src/config/db.connection.js
+++ src/config/db.connection.js
@@ -2,24 +2,38 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 const db = require("./conf");
 
-const sequelize = new Sequelize(
-  db.database.name,
-  db.database.username,
-  db.database.password,
-  {
-    host: db.database.host,
-    dialect: db.database.dialect,
-    dialect: "mysql",
-    logging: false,
-    port: db.database.port,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  }
-);
+// const sequelize = new Sequelize(
+//   db.database.name,
+//   db.database.username,
+//   db.database.password,
+//   {
+//     host: db.database.host,
+//     dialect: db.database.dialect,
+//     dialect: "mysql",
+//     logging: false,
+//     port: db.database.port,
+//   }
+// );
+const sequelize = new Sequelize({
+  host: db.database.host,
+  dialect: "mysql",
+  dialectModule: require("mysql2"),
+  username: db.database.username,
+  password: db.database.password,
+  database: db.database.name,
+  port: db.database.port,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+});
 
 exports.connect = async () => {
   try {
